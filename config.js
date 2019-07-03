260d56e24ab3469224a4336728ea04ae783089a6
potato <internet221102@gmail.com>
2019-07-03 22:36:57 +0800

    version7

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 try {
-    switch (process.env.NODE_ENV) {
+    //switch (process.env.NODE_ENV) {
+    switch ('production') {
         case 'development':
             process.env.DB_URL = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOSTNAME}:${process.env.PG_PORT}/${process.env.PG_DB_NAME}`;
             console.log("In developing");
             break;
         default:
-            process.env.DB_URL = `postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
-            console.log(process.env.DB_URL)
+            //process.env.DB_URL = `postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+            process.env.DB_URL = `postgres://potatodb:asd12125@mydb.cjwecs7ktrgk.ap-northeast-1.rds.amazonaws.com:5432/playdb`;
             break;
     }
 } catch (err) {
